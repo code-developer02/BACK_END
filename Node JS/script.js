25ae67182da13c59f6198bd1198e7039f6b0d78e
Mann <mannkansagara0709@gmail.com>
2024-07-24 00:56:26 +0530

    .

--- Node JS/script.js
+++ Node JS/script.js
@@ -1,1 +1,56 @@
+// File System .....
 
+// To use the callback and sync APls:
+const fs = require( 'node:fs');
+
+// CALLBACK API....
+
+// Writefile
+//fs.writeFile(file, data[, options], callback) .   callback=function
+
+// fs.writeFile("hey.txt","How are you?",function(err){
+//     if(err) console.error (err);
+//     else console.log("done");
+// })
+
+
+//Append
+//fs.appendFile(path, datal, options], callback)
+
+// fs.appendFile("hey.txt"," I am Fine..",function(err){
+//     if(err) console.error (err);
+//     else console.log("done");
+// })
+
+
+//Rename
+//fs.rename(oldPath, newPath, callback)
+
+// fs.rename("hey.txt", "hello.txt", function(err){
+//     if(err) console.error(err);
+//     else console. log ("done");
+//     })
+
+//Copy
+//fs.copyFile(src, dest|, mode], callback)
+
+// fs.copyFile("hello.txt", "./copy/chacha.txt", function(err){
+//     if(err) console.error(err);
+//     else console. log ("done");
+// })
+
+//Unlink --> Delete the file
+//fs.unlink(path, callback)
+
+// fs.unlink("hello.txt", function(err){
+//     if(err) console.error (err);
+//     else console. log ("removed");
+// })
+
+// Remove Directory
+// fs.rmdir(path{,option}, callback)
+
+// fs.rm("./copy",{recursive : true},function(err){
+//     if(err) console.error(err);
+//     else console.log("removed");
+// })
